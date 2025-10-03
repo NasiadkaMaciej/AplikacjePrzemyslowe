ced4ed65e1d07ff5665a3252343e7819e9acf191
NasiadkaMaciej <maciej.nasiadka@gmail.com>
2025-10-03 11:09:13 +0200

    Initial commit

---
+++ src/main/java/MaciejNasiadka/Zadanie1/Zadanie1Application.java
@@ -0,0 +1,14 @@
+package MaciejNasiadka.Zadanie1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zadanie1Application.class, args);
+		System.err.println("Uruchomiono aplikację Zadanie1");
+	}
+
+}
